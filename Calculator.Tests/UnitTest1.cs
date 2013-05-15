eba1dc13c59bc3209487bd58f0aab65fa5b523eb
Mark Lamley <marksl.git@gmail.com>
2013-05-14 21:38:07 -0700

    Initial checkin

---
+++ Calculator.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void Add_TwoNumbers_ReturnsSum()
+        {
+            var calculator = new Calculator();
+            
+            var sum = calculator.Add(1, 2);
+
+            Assert.AreEqual(3, sum);
+        }
+    }
+}
